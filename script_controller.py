96d0dd8bde84f10ad80747a8cd7515099eac0365
Egor Krivov <egor.krivov@frtk.ru>
2016-06-15 23:53:40 +0300

    Simple script to control unstable calculations

---
+++ script_controller.py
@@ -0,0 +1,29 @@
+#!/bin/python
+import argparse
+import time
+import subprocess
+
+if __name__ == '__main__':
+
+    # Parsing
+    parser = argparse.ArgumentParser('Script controller')
+    parser.add_argument('script', type=str, help='script path')
+
+    parser.add_argument('-a', '--attempts', type=int, default=100,
+                        help='maximum number of script restarts')
+
+    args = parser.parse_args()
+
+    script = args.script
+    attempts = args.attempts
+
+    try:
+        r = 1
+        for attempt in range(attempts):
+            start = time.time()
+            r = subprocess.run(['python', script]).returncode
+            end = time.time()
+            print('return code is {}, time {}'.format(r, end-start))
+
+    except KeyboardInterrupt:
+        print('\nInterrupted by keyboard')
